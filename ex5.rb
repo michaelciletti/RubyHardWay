be8ae71399d2d0947256cdbecbf0c9ce96943d09
michaelciletti <michaelciletti@gmail.com>
2017-01-19 09:42:19 -0500

    converted standard measurements

--- ex5.rb
+++ ex5.rb
@@ -1,14 +1,14 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
+my_height = 74 * 2.54 # cm
+my_weight = 180 / 0.45 # kg
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "He's #{my_height} centimeters tall."
+puts "He's #{my_weight} kilograms heavy."
 puts "Actualy that's not too heavy."
 puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "his teeth are usually #{my_teeth} depending on the coffee."
