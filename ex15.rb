63e7067115c2c0f2de4b38fadf193fe1b33cc495
michaelciletti <michaelciletti@gmail.com>
2017-01-22 13:18:32 -0500

    adding ex16

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,15 @@
 filename = ARGV.first
 
 txt = open(filename)
-#^^^^opens the file
+#^^^^accesses the file but does not output it...At least I think that is what it does!
 
 puts "Here's your file #{filename}:"   #Prints the string along with inserting the filename
 print txt.read
 #^^^^^This prints out my ex15_sample.txt file
 
 print "Type the filename again: "
-file_again = $stdin.gets.chomp
+file_again = $stdin.gets.chomp  #Prints line above and prompts for filename
 
-txt_again = open(file_again)
+txt_again = open(file_again) #makes txt_again a variable equaling to be opened and then printed in the command below
 
 print txt_again.read
